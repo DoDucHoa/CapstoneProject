30f228f096c005c511e46af07c2c9a6dc6c65d51
DoDucHoa <hhoa0978@gmail.com>
2022-04-27 18:41:13 +0700

    [Backend]: update Configure in Startup.cs for routing

--- PawNClaw.Backend/PawNClaw.API/Startup.cs
+++ PawNClaw.Backend/PawNClaw.API/Startup.cs
@@ -74,6 +74,9 @@ namespace PawNClaw.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PawNClaw.API v1"));
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "PawNClaw.API v1"); c.RoutePrefix = string.Empty; });
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
