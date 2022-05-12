ba5d5e1dbd945dceb2c0d44026943841cc34444c
NgHLong-now <ng.hoang.long10320@gmail.com>
2022-05-12 09:35:35 +0700

    [Backend]-Pet Controller and Service
    - Add new function get pet by customer Id
    - Admin only get Mod (required includeProperties)

---
+++ PawNClaw.Backend/PawNClaw.API/Controllers/PetController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PawNClaw.Business.Services;
+using PawNClaw.Data.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawNClaw.API.Controllers
+{
+    [Route("api/pets")]
+    [ApiController]
+    [Authorize]
+    public class PetController : ControllerBase
+    {
+        private readonly PetService _petService;
+        public PetController(PetService petService)
+        {
+            _petService = petService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPetsByCusId(int Id, [FromQuery] PagingParameter paging)
+        {
+            var data = _petService.GetsPetByCusId(Id, paging);
+            var metadata = new
+            {
+                data.TotalCount,
+                data.PageSize,
+                data.CurrentPage,
+                data.TotalPages,
+                data.HasNext,
+                data.HasPrevious
+            };
+            return Ok(new { data, metadata });
+        }
+    }
+}
