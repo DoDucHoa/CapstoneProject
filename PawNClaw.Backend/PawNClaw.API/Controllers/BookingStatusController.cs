43d020d81f4cc395726af6f9f87b771baf0db25c
NgHLong-now <ng.hoang.long10320@gmail.com>
2022-06-02 20:26:06 +0700

    [Backend]-Update Pet For Staff And Get Booking Status

---
+++ PawNClaw.Backend/PawNClaw.API/Controllers/BookingStatusController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PawNClaw.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PawNClaw.API.Controllers
+{
+    [Route("api/bookingstatuses")]
+    [ApiController]
+    [Authorize]
+    public class BookingStatusController : ControllerBase
+    {
+        BookingStatusService _bookingStatusService;
+
+        public BookingStatusController(BookingStatusService bookingStatusService)
+        {
+            _bookingStatusService = bookingStatusService;
+        }
+
+        [HttpGet]
+        public IActionResult GetBookingStatuses()
+        {
+            var data = _bookingStatusService.GetBookingStatuses();
+
+            return Ok(data);
+        }
+    }
+}
