1a8f21c0d55f47859c305407b5214caa1dd347fe
NgHLong-now <ng.hoang.long10320@gmail.com>
2022-06-15 17:20:14 +0700

    [Backend]-Fix Update Booking Detail
    - Cal Total Price when Update

--- PawNClaw.Backend/PawNClaw.Business/Services/BookingDetailService.cs
+++ PawNClaw.Backend/PawNClaw.Business/Services/BookingDetailService.cs
@@ -1,4 +1,5 @@
-﻿using PawNClaw.Data.Const;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using PawNClaw.Data.Const;
 using PawNClaw.Data.Database;
 using PawNClaw.Data.Interface;
 using PawNClaw.Data.Parameter;
@@ -16,15 +17,21 @@ namespace PawNClaw.Business.Services
         IPetBookingDetailRepository _petBookingDetailRepository;
         IPetRepository _petRepository;
         ICageRepository _cageRepository;
+        IBookingRepository _bookingRepository;
+
+        private readonly ApplicationDbContext _db;
 
         public BookingDetailService(IBookingDetailRepository bookingDetailRepository,
             IPetBookingDetailRepository petBookingDetailRepository, IPetRepository petRepository,
-            ICageRepository cageRepository)
+            ICageRepository cageRepository, IBookingRepository bookingRepository,
+            ApplicationDbContext db)
         {
             _bookingDetailRepository = bookingDetailRepository;
             _petBookingDetailRepository = petBookingDetailRepository;
             _petRepository = petRepository;
             _cageRepository = cageRepository;
+            _bookingRepository = bookingRepository;
+            _db = db;
         }
 
         //Get By Booking Id
@@ -44,9 +51,10 @@ namespace PawNClaw.Business.Services
         }
 
         //Update New Cage For Booking Detail
-        public bool UpdateCageOfBookingDetail(UpdateBookingDetailParameter updateBookingDetailParameter)
+        public async Task<bool> UpdateCageOfBookingDetail(UpdateBookingDetailParameter updateBookingDetailParameter)
         {
-            var value = _bookingDetailRepository.GetBookingDetail(updateBookingDetailParameter.BookingId, updateBookingDetailParameter.Line);
+            var value = _bookingDetailRepository.GetBookingDetail(updateBookingDetailParameter.BookingId,
+                updateBookingDetailParameter.Line);
 
             //var petbookingdetails = _petBookingDetailRepository.GetAll(x => x.BookingId == updateBookingDetailParameter.BookingId
             //                                                        && x.Line == updateBookingDetailParameter.Line);
@@ -81,16 +89,54 @@ namespace PawNClaw.Business.Services
             value.Price = updateBookingDetailParameter.Price;
             value.CageCode = updateBookingDetailParameter.CageCode;
             value.Note = updateBookingDetailParameter.Note;
+            using (IDbContextTransaction transaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    _bookingDetailRepository.Update(value);
+                    await _bookingDetailRepository.SaveDbChangeAsync();
 
-            try
-            {
-                _bookingDetailRepository.Update(value);
-                _bookingDetailRepository.SaveDbChange();
-                return true;
-            }
-            catch
-            {
-                throw new Exception();
+                    var booking = _bookingRepository.GetBookingForCustomer(updateBookingDetailParameter.BookingId);
+
+                    var serviceOrders = booking.ServiceOrders;
+
+                    var supplyOrders = booking.SupplyOrders;
+
+                    var bookingDetails = booking.BookingDetails;
+
+                    decimal Price = 0;
+
+                    foreach (var serviceOrder in serviceOrders)
+                    {
+                        Price = (decimal)(Price + serviceOrder.TotalPrice);
+                    }
+
+                    foreach (var supplyOrder in supplyOrders)
+                    {
+                        Price = (decimal)(Price + supplyOrder.TotalPrice);
+                    }
+
+                    foreach (var bookingDetail in bookingDetails)
+                    {
+                        Price = (decimal)(Price + bookingDetail.Price);
+                    }
+
+                    var bookingToDb = _bookingRepository.Get(booking.Id);
+
+                    bookingToDb.SubTotal = Price;
+                    bookingToDb.Total = Price;
+
+                    _bookingRepository.Update(bookingToDb);
+                    await _bookingRepository.SaveDbChangeAsync();
+
+                    transaction.Commit();
+                    return true;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw new Exception();
+                }
             }
         }
     }
