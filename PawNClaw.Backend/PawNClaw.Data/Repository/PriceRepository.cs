3885780163a141c24388c0390c43064dae7459fa
NgHLong-now <ng.hoang.long10320@gmail.com>
2022-06-30 16:46:31 +0700

    [Backend]-Fix Check Price Of Booking

--- PawNClaw.Backend/PawNClaw.Data/Repository/PriceRepository.cs
+++ PawNClaw.Backend/PawNClaw.Data/Repository/PriceRepository.cs
@@ -26,15 +26,15 @@ namespace PawNClaw.Data.Repository
             DateTime _endBooking = DateTime.ParseExact(EndBooking, SearchConst.DateFormat,
                                        System.Globalization.CultureInfo.InvariantCulture);
 
-            DateTime _aStarBooking = _startBooking.AddDays(1);
-            DateTime _bEndBooking = _endBooking.AddDays(-1);
+            //TimeSpan diffOfDates = _endBooking.TimeOfDay.Subtract(_startBooking.TimeOfDay);
+
 
             decimal TotalPrice = 0;
             decimal? unitPrice = null;
             try
             {
 
-                for (var dt = _aStarBooking; dt.Date <= _bEndBooking.Date; dt = dt.AddDays(1))
+                for (var dt = _startBooking; dt.Date <= _endBooking.Date; dt = dt.AddDays(1))
                 {
                     //Check if have any price is specify date
                     if (query.Any(x => x.PriceTypeCode.Equals("PRICE-004")))
@@ -45,7 +45,7 @@ namespace PawNClaw.Data.Repository
                         //Check if have specify date price and the date booking equal with that day
                         if (unitPrice != null)
                         {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice * 24);
+                            TotalPrice = (decimal)(TotalPrice + unitPrice);
                             continue;
                         }
                     }
@@ -57,7 +57,7 @@ namespace PawNClaw.Data.Repository
                         unitPrice = null;
                         if (unitPrice != null)
                         {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice * 24);
+                            TotalPrice = (decimal)(TotalPrice + unitPrice);
                             continue;
                         }
                     }
@@ -67,126 +67,125 @@ namespace PawNClaw.Data.Repository
                         {
                             unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-002"))
                                     .FirstOrDefault().UnitPrice;
-                            TotalPrice = (decimal)(TotalPrice + unitPrice * 24);
+                            TotalPrice = (decimal)(TotalPrice + unitPrice);
                             continue;
                         }
                     }
 
                     unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-001"))
                             .FirstOrDefault().UnitPrice;
-                    TotalPrice = (decimal)(TotalPrice + unitPrice * 24);
+                    TotalPrice = (decimal)(TotalPrice + unitPrice);
                     continue;
                 }
 
 
                 //****************************************************************************
                 //Here Check First Date Booking
-                if (query.Any(x => x.PriceTypeCode.Equals("PRICE-004")))
-                {
-                    unitPrice = query.Where(x => x.DateFrom <= _startBooking.Date && x.DateTo >= _startBooking.Date)
-                                                .FirstOrDefault().UnitPrice;
+            //    if (query.Any(x => x.PriceTypeCode.Equals("PRICE-004")))
+            //    {
+            //        unitPrice = query.Where(x => x.DateFrom <= _startBooking.Date && x.DateTo >= _startBooking.Date)
+            //                                    .FirstOrDefault().UnitPrice;
 
-                    //Check if have specify date price and the date booking equal with that day
-                    if (unitPrice != null)
-                    {
-                        TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
-                        if (_startBooking.Minute == 30)
-                        {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                        }
-                    }
-                }
-                //Here is no type specify date
-                if (query.Any(x => x.PriceTypeCode.Equals("PRICE-003")))
-                {
-                    //Here check the booking date is equal with the day of week
-                    //Xu ly check thu o day hien tai null
-                    unitPrice = null;
-                    if (unitPrice != null)
-                    {
-                        TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
-                        if (_startBooking.Minute == 30)
-                        {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                        }
-                    }
-                }
-                if (query.Any(x => x.PriceTypeCode.Equals("PRICE-002")))
-                {
-                    if (_startBooking.DayOfWeek == DayOfWeek.Saturday || _startBooking.DayOfWeek == DayOfWeek.Sunday)
-                    {
-                        unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-002"))
-                                .FirstOrDefault().UnitPrice;
-                        TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
-                        if (_startBooking.Minute == 30)
-                        {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                        }
-                    }
-                }
+            //        //Check if have specify date price and the date booking equal with that day
+            //        if (unitPrice != null)
+            //        {
+            //            TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
+            //            if (_startBooking.Minute == 30)
+            //            {
+            //                TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //            }
+            //        }
+            //    }
+            //    //Here is no type specify date
+            //    if (query.Any(x => x.PriceTypeCode.Equals("PRICE-003")))
+            //    {
+            //        //Here check the booking date is equal with the day of week
+            //        //Xu ly check thu o day hien tai null
+            //        unitPrice = null;
+            //        if (unitPrice != null)
+            //        {
+            //            TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
+            //            if (_startBooking.Minute == 30)
+            //            {
+            //                TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //            }
+            //        }
+            //    }
+            //    if (query.Any(x => x.PriceTypeCode.Equals("PRICE-002")))
+            //    {
+            //        if (_startBooking.DayOfWeek == DayOfWeek.Saturday || _startBooking.DayOfWeek == DayOfWeek.Sunday)
+            //        {
+            //            unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-002"))
+            //                    .FirstOrDefault().UnitPrice;
+            //            TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
+            //            if (_startBooking.Minute == 30)
+            //            {
+            //                TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //            }
+            //        }
+            //    }
 
-                unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-001"))
-                        .FirstOrDefault().UnitPrice;
-                TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
-                if (_startBooking.Minute == 30)
-                {
-                    TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                }
+            //    unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-001"))
+            //            .FirstOrDefault().UnitPrice;
+            //    TotalPrice = (decimal)(TotalPrice + unitPrice * (24 - _startBooking.Hour));
+            //    if (_startBooking.Minute == 30)
+            //    {
+            //        TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //    }
 
-                //****************************************************************************************************
-                //Here Check Last Date Booking
-                if (query.Any(x => x.PriceTypeCode.Equals("PRICE-004")))
-                {
-                    unitPrice = query.Where(x => x.DateFrom <= _endBooking.Date && x.DateTo >= _endBooking.Date)
-                                                .FirstOrDefault().UnitPrice;
+            //    //****************************************************************************************************
+            //    //Here Check Last Date Booking
+            //    if (query.Any(x => x.PriceTypeCode.Equals("PRICE-004")))
+            //    {
+            //        unitPrice = query.Where(x => x.DateFrom <= _endBooking.Date && x.DateTo >= _endBooking.Date)
+            //                                    .FirstOrDefault().UnitPrice;
 
-                    //Check if have specify date price and the date booking equal with that day
-                    if (unitPrice != null)
-                    {
-                        TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
-                        if (_endBooking.Minute == 30)
-                        {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                        }
-                    }
-                }
-                //Here is no type specify date
-                if (query.Any(x => x.PriceTypeCode.Equals("PRICE-003")))
-                {
-                    //Here check the booking date is equal with the day of week
-                    //Xu ly check thu o day hien tai null
-                    unitPrice = null;
-                    if (unitPrice != null)
-                    {
-                        TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
-                        if (_endBooking.Minute == 30)
-                        {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                        }
-                    }
-                }
-                if (query.Any(x => x.PriceTypeCode.Equals("PRICE-002")))
-                {
-                    if (_endBooking.DayOfWeek == DayOfWeek.Saturday || _endBooking.DayOfWeek == DayOfWeek.Sunday)
-                    {
-                        unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-002"))
-                                .FirstOrDefault().UnitPrice;
-                        TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
-                        if (_endBooking.Minute == 30)
-                        {
-                            TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                        }
-                    }
-                }
+            //        //Check if have specify date price and the date booking equal with that day
+            //        if (unitPrice != null)
+            //        {
+            //            TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
+            //            if (_endBooking.Minute == 30)
+            //            {
+            //                TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //            }
+            //        }
+            //    }
+            //    //Here is no type specify date
+            //    if (query.Any(x => x.PriceTypeCode.Equals("PRICE-003")))
+            //    {
+            //        //Here check the booking date is equal with the day of week
+            //        //Xu ly check thu o day hien tai null
+            //        unitPrice = null;
+            //        if (unitPrice != null)
+            //        {
+            //            TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
+            //            if (_endBooking.Minute == 30)
+            //            {
+            //                TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //            }
+            //        }
+            //    }
+            //    if (query.Any(x => x.PriceTypeCode.Equals("PRICE-002")))
+            //    {
+            //        if (_endBooking.DayOfWeek == DayOfWeek.Saturday || _endBooking.DayOfWeek == DayOfWeek.Sunday)
+            //        {
+            //            unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-002"))
+            //                    .FirstOrDefault().UnitPrice;
+            //            TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
+            //            if (_endBooking.Minute == 30)
+            //            {
+            //                TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //            }
+            //        }
+            //    }
 
-                unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-001"))
-                        .FirstOrDefault().UnitPrice;
-                TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
-                if (_endBooking.Minute == 30)
-                {
-                    TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
-                }
-                
+            //    unitPrice = query.Where(x => x.PriceTypeCode.Equals("PRICE-001"))
+            //            .FirstOrDefault().UnitPrice;
+            //    TotalPrice = (decimal)(TotalPrice + unitPrice * _endBooking.Hour);
+            //    if (_endBooking.Minute == 30)
+            //    {
+            //        TotalPrice = (decimal)(TotalPrice + unitPrice / 2);
+            //    }
             }
             catch
             {
