2138f359d88fd0557c12aa2135fc606c997d1b3e
DoDucHoa <hhoa0978@gmail.com>
2022-07-11 21:25:21 +0700

    [Frontend]: develope Booking List screen

--- PawNClaw.Frontend/src/pages/dashboard/BookingList/BookingList.js
+++ PawNClaw.Frontend/src/pages/dashboard/BookingList/BookingList.js
@@ -1,9 +1,8 @@
-import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 // @mui
-import { Button, Card, Container, Typography } from '@mui/material';
+import { Button, Card, Container, Grid, MenuItem, TextField } from '@mui/material';
+import DatePicker from '@mui/lab/DatePicker';
 
 // routes
 import { PATH_DASHBOARD } from '../../../routes/paths';
@@ -14,11 +13,17 @@ import useSettings from '../../../hooks/useSettings';
 // components
 import Page from '../../../components/Page';
 import HeaderBreadcrumbs from '../../../components/HeaderBreadcrumbs';
-import Iconify from '../../../components/Iconify';
+
+// config
+import { BOOKING_STATUS } from '../../../config';
 
 // ----------------------------------------------------------------------
 
 export default function BookingList() {
+  const [fromDate, setFromDate] = useState('');
+  const [toDate, setToDate] = useState('');
+  const [bookingStatus, setBookingStatus] = useState(3);
+
   const { themeStretch } = useSettings();
 
   return (
@@ -30,8 +35,62 @@ export default function BookingList() {
         />
 
         <Card sx={{ p: 3 }}>
-          <Typography>Ngày từ</Typography>
-          
+          <Grid container spacing={3}>
+            <Grid item md={3}>
+              <DatePicker
+                label="Từ ngày"
+                value={fromDate}
+                onChange={(newValue) => {
+                  setFromDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} fullWidth />}
+              />
+            </Grid>
+            <Grid item md={3}>
+              <DatePicker
+                label="Đến ngày"
+                value={toDate}
+                onChange={(newValue) => {
+                  setToDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} fullWidth />}
+              />
+            </Grid>
+            {BOOKING_STATUS.length > 0 && (
+              <Grid item xs={8} md={3}>
+                <TextField
+                  label="Trạng thái booking"
+                  value={bookingStatus}
+                  onChange={(event) => setBookingStatus(event.target.value)}
+                  select
+                  fullWidth
+                  SelectProps={{ native: false }}
+                  InputLabelProps={{ shrink: true }}
+                >
+                  {BOOKING_STATUS.map((option) => (
+                    <MenuItem
+                      key={option.id}
+                      value={option.id}
+                      sx={{
+                        mx: 1,
+                        my: 0.5,
+                        borderRadius: 0.75,
+                        typography: 'body2',
+                        textTransform: 'capitalize',
+                      }}
+                    >
+                      {option.name}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+            )}
+            <Grid item md={3} sx={{ alignSelf: 'center' }}>
+              <Button variant="contained" size="large" fullWidth>
+                Tìm kiếm
+              </Button>
+            </Grid>
+          </Grid>
         </Card>
       </Container>
     </Page>
