3fd94e154606b890a12e03d29cce7f308d35e35a
NgHLong-now <longnguyen.work.hcm@gmail.com>
2022-07-20 21:40:06 +0700

    [Backend]-Sponsor Banner

---
+++ PawNClaw.Backend/PawNClaw.Business/Services/SponsorBannerService.cs
@@ -0,0 +1,26 @@
+﻿using PawNClaw.Data.Database;
+using PawNClaw.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawNClaw.Business.Services
+{
+    public class SponsorBannerService
+    {
+        ISponsorBannerRepository _sponsorBannerRepository;
+
+        public SponsorBannerService(ISponsorBannerRepository sponsorBannerRepository)
+        {
+            _sponsorBannerRepository = sponsorBannerRepository;
+        }
+
+        public IEnumerable<SponsorBanner> GetSponsorBanners()
+        {
+            DateTime today = DateTime.Today;
+            return _sponsorBannerRepository.GetAll(x => x.Status == true && ((DateTime)x.StartDate).Date <= today && ((DateTime)x.EndDate).Date >= today);
+        }
+    }
+}
