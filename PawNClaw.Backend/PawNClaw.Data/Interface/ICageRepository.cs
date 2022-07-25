195c1d67652688514738fcc8fec5a5e8be5675ac
vhnguyen712 <vhnguyen2k@gmail.com>
2022-07-25 10:13:44 +0700

    [Backend] - Check cages can be shifted when get

--- PawNClaw.Backend/PawNClaw.Data/Interface/ICageRepository.cs
+++ PawNClaw.Backend/PawNClaw.Data/Interface/ICageRepository.cs
@@ -18,5 +18,7 @@ namespace PawNClaw.Data.Interface
         public Cage GetCage(string Code, int CenterId);
 
         public bool UpdateCageStatus(List<String> CageCodes, int centerId);
+
+        public IEnumerable<Cage> GetCages(int CenterId);
     }
 }
