6cbbd8ccb746e4ded78c3aacc9d83f624eba6258
NgHLong-now <longnguyen.work.hcm@gmail.com>
2022-07-26 11:28:06 +0700

    [Backend]-Service CRUD

--- PawNClaw.Backend/PawNClaw.API/Controllers/ServicesController.cs
+++ PawNClaw.Backend/PawNClaw.API/Controllers/ServicesController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PawNClaw.Business.Services;
+using PawNClaw.Data.Database;
 using PawNClaw.Data.Helper;
+using PawNClaw.Data.Parameter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +24,7 @@ namespace PawNClaw.API.Controllers
             _serviceServices = serviceServices;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("center/{id}")]
         public IActionResult GetServicesOfCenter(int id, PagingParameter paging)
         {
             var data = _serviceServices.GetServices(id, paging);
@@ -36,6 +38,45 @@ namespace PawNClaw.API.Controllers
                 data.HasPrevious
             };
             return Ok(new { data, metadata });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetService(int id)
+        {
+            try
+            {
+                return Ok(_serviceServices.GetService(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateService([FromBody] CreateServiceParameter createServiceParameter)
+        {
+            try
+            {
+                return Ok(_serviceServices.CreateService(createServiceParameter.service, createServiceParameter.servicePrice));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateService([FromBody] Service service)
+        {
+            try
+            {
+                return Ok(_serviceServices.UpdateService(service));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
     }
 }
