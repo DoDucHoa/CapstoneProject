bab88688b91656fdbf9bec67ebc560c20af07a3d
vhnguyen712 <vhnguyen2k@gmail.com>
2022-07-27 11:35:15 +0700

    [Backend] - Minor changes

--- PawNClaw.Backend/PawNClaw.Data/Parameter/ServiceParameter.cs
+++ PawNClaw.Backend/PawNClaw.Data/Parameter/ServiceParameter.cs
@@ -24,6 +24,20 @@ namespace PawNClaw.Data.Parameter
         public string includeProperties { get; set; } = null;
     }
 
+    public class CreateService
+    {
+        public int? Id { get; set; }
+        public string Description { get; set; }
+        public decimal? DiscountPrice { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        public int? CreateUser { get; set; }
+        public int? ModifyUser { get; set; }
+        public bool? Status { get; set; }
+        public int CenterId { get; set; }
+        public string Name { get; set; }
+    }
+
     public class CreateServiceParameter
     {
         public CreateService service { get; set; }
