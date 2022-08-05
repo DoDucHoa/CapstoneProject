97991e422e1f6a480b061f93e4738a1286bf2669
NgHLong-now <longnguyen.work.hcm@gmail.com>
2022-08-05 23:27:52 +0700

    [Backend]-Admin Dashboard

---
+++ PawNClaw.Backend/PawNClaw.Business/Services/AdminDashboardService.cs
@@ -0,0 +1,39 @@
+﻿using PawNClaw.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawNClaw.Business.Services
+{
+    public class AdminDashboardService
+    {
+        IPetCenterRepository _petCenterRepository;
+        IBrandRepository _brandRepository;
+        IAccountRepository _accountRepository;
+
+        public AdminDashboardService(IPetCenterRepository petCenterRepository, IBrandRepository brandRepository,
+            IAccountRepository accountRepository)
+        {
+            _petCenterRepository = petCenterRepository;
+            _brandRepository = brandRepository;
+            _accountRepository = accountRepository;
+        }
+
+        public int CenterAvailable()
+        {
+            return _petCenterRepository.GetAll(x => x.Status == true).Count();
+        }
+
+        public int BrandAvailable()
+        {
+            return _brandRepository.GetAll(x => x.Status == true).Count();
+        }
+
+        public int CustomerAvailable()
+        {
+            return _accountRepository.GetAll(includeProperties: "Customer", filter: x => x.Status == true).Where(x => x.Customer != null).Count();
+        }
+    }
+}
