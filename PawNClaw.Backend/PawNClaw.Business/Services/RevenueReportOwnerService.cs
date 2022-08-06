0021d999b5d8c8e9f079decfb4ecbbdba001df92
NgHLong-now <longnguyen.work.hcm@gmail.com>
2022-08-06 18:56:15 +0700

    [Backend]-Fix Dashboard
    - Fix Search
    - Fix Confirm booking

--- PawNClaw.Backend/PawNClaw.Business/Services/RevenueReportOwnerService.cs
+++ PawNClaw.Backend/PawNClaw.Business/Services/RevenueReportOwnerService.cs
@@ -1,4 +1,5 @@
-﻿using PawNClaw.Data.Interface;
+﻿using PawNClaw.Data.Database;
+using PawNClaw.Data.Interface;
 using PawNClaw.Data.Parameter;
 using System;
 using System.Collections.Generic;
@@ -19,19 +20,30 @@ namespace PawNClaw.Business.Services
             _cageRepository = cageRepository;
         }
 
-        public BookingCount BookingCount(int centerId)
+        public BookingCount BookingCount(int centerId, DateTime? from, DateTime? to)
         {
-            var bookingC = _bookingRepository.GetAll(x => x.CenterId == centerId).Count();
+            var booking = _bookingRepository.GetAll(x => x.CenterId == centerId);
+
+            if (from != null && to != null)
+            {
+                booking = booking.Where(x => (x.StartBooking >= from && x.StartBooking <= to) || (x.EndBooking >= from && x.EndBooking <= to));
+            }
+
             BookingCount bookingCount = new BookingCount()
             {
-                TotalBooking = bookingC
+                TotalBooking = booking.Count()
             };
             return bookingCount;
         }
 
-        public BookingCountWithStatus BookingCountWithStatus(int centerId)
+        public BookingCountWithStatus BookingCountWithStatus(int centerId, DateTime? from, DateTime? to)
         {
             var bookings = _bookingRepository.GetAll(x => x.CenterId == centerId);
+
+            if (from != null && to != null)
+            {
+                bookings = bookings.Where(x => (x.StartBooking >= from && x.StartBooking <= to) || (x.EndBooking >= from && x.EndBooking <= to));
+            }
 
             BookingCountWithStatus bookingCount = new BookingCountWithStatus()
             {
@@ -44,16 +56,45 @@ namespace PawNClaw.Business.Services
             return bookingCount;
         }
 
-        public TotalCageOfCenter TotalCageOfCenter(int centerId)
+        public TotalCageOfCenter TotalCageOfCenter(int centerId, DateTime? from, DateTime? to)
         {
-            var cageC = _cageRepository.GetAll(x => x.CenterId == centerId && x.Status == true).Count();
+            var cages = _cageRepository.GetAll(x => x.CenterId == centerId && x.Status == true, includeProperties: "BookingDetails,BookingDetails.Booking");
 
             TotalCageOfCenter totalCageOfCenter = new TotalCageOfCenter()
             {
-                TotalCage = cageC
+                TotalCage = cages.Count(),
+                TotalCageAvailable = cages.Where(x => x.BookingDetails.Any(detail => 
+                                !(((DateTime.Compare((DateTime)from, (DateTime)detail.Booking.StartBooking) <= 0
+                                && DateTime.Compare((DateTime)to, (DateTime)detail.Booking.EndBooking) >= 0)
+                                ||
+                                (DateTime.Compare((DateTime)from, (DateTime)detail.Booking.StartBooking) >= 0
+                                && DateTime.Compare((DateTime)from, (DateTime)detail.Booking.EndBooking) < 0)
+                                ||
+                                (DateTime.Compare((DateTime)to, (DateTime)detail.Booking.StartBooking) > 0
+                                && DateTime.Compare((DateTime)to, (DateTime)detail.Booking.EndBooking) <= 0))
+                                && (detail.Booking.StatusId == 1 || detail.Booking.StatusId == 2)))).Count()
             };
 
             return totalCageOfCenter;
+        }
+
+        public IEnumerable<Cage> CageFreeOfCenter(int centerId, DateTime? from, DateTime? to)
+        {
+            var cages = _cageRepository.GetAll(x => x.CenterId == centerId && x.Status == true, includeProperties: "BookingDetails,BookingDetails.Booking");
+
+            cages = cages.Where(x => x.BookingDetails.Any(detail =>
+                                !(((DateTime.Compare((DateTime)from, (DateTime)detail.Booking.StartBooking) <= 0
+                                && DateTime.Compare((DateTime)to, (DateTime)detail.Booking.EndBooking) >= 0)
+                                ||
+                                (DateTime.Compare((DateTime)from, (DateTime)detail.Booking.StartBooking) >= 0
+                                && DateTime.Compare((DateTime)from, (DateTime)detail.Booking.EndBooking) < 0)
+                                ||
+                                (DateTime.Compare((DateTime)to, (DateTime)detail.Booking.StartBooking) > 0
+                                && DateTime.Compare((DateTime)to, (DateTime)detail.Booking.EndBooking) <= 0))
+                                && (detail.Booking.StatusId == 1 || detail.Booking.StatusId == 2))));
+            
+
+            return cages.ToList();
         }
 
         public IncomeOfCenter IncomeOfCenter(int centerId)
@@ -73,6 +114,27 @@ namespace PawNClaw.Business.Services
                                                 && ((DateTime)x.StartBooking).Year == today.Year)
                                         .Sum(x => x.Total)) / (bookings.Where(x => ((DateTime)x.StartBooking).Month == today.AddMonths(-1).Month
                                                 && ((DateTime)x.StartBooking).Year == today.Year)
+                                        .Sum(x => x.Total)))) - 1
+            };
+
+            return incomeOfCenter;
+        }
+
+        public IncomeOfCenter IncomeOfCenterCustomeMonth(int centerId, DateTime date)
+        {
+            var bookings = _bookingRepository.GetAll(x => x.CenterId == centerId && x.StatusId == 3);
+
+            IncomeOfCenter incomeOfCenter = new IncomeOfCenter()
+            {
+                IncomeOfMonth = bookings.Where(x => ((DateTime)x.StartBooking).Month == date.Month
+                                                && ((DateTime)x.StartBooking).Year == date.Year)
+                                        .Sum(x => x.Total),
+                IncomeOfYear = bookings.Where(x => ((DateTime)x.StartBooking).Year == date.Year)
+                                        .Sum(x => x.Total),
+                PercentWithLastMonth = (float?)(((bookings.Where(x => ((DateTime)x.StartBooking).Month == date.Month
+                                                && ((DateTime)x.StartBooking).Year == date.Year)
+                                        .Sum(x => x.Total)) / (bookings.Where(x => ((DateTime)x.StartBooking).Month == date.AddMonths(-1).Month
+                                                && ((DateTime)x.StartBooking).Year == date.Year)
                                         .Sum(x => x.Total)))) - 1
             };
 
