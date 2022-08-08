35bc0c7047dec8dbc3cc6667b3925050b980994f
vhnguyen712 <vhnguyen2k@gmail.com>
2022-08-09 02:16:05 +0700

    [Backend] - Add logservice

---
+++ PawNClaw.Backend/PawNClaw.Data/Parameter/ActionLogs.cs
@@ -0,0 +1,18 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawNClaw.Data.Parameter
+{
+    public class ActionLogsParameter
+    {
+        public long Id { get; set; }
+        public string Target { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public Timestamp Time { get; set; }
+    }
+}
